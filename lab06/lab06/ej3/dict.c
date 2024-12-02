2cb4926b0e858149620d08c9e2f23d840de487c7
gfranicevich <gfranicevich@mi.unc.edu.ar>
2024-12-01 22:22:12 -0300

    lab 06

--- lab06/lab06/ej3/dict.c
+++ lab06/lab06/ej3/dict.c
@@ -13,90 +13,80 @@ struct _node_t {
     value_t value;
 };
 
+typedef struct s_tuple{
+    key_t key;
+    value_t value;
+} tuple;
+
+
 static bool invrep(dict_t d) {
-    if (d == NULL ) return true;
-    dict_t lft = d->left;
-    if (lft != NULL){
-        if (!key_less(lft->key, d->key)) {
-            return false;
+    dict_t rgt = d == NULL ? NULL : d->right;
+    dict_t lft = d == NULL ? NULL : d->left;
+
+    //  si tiene algún hijo
+    if(d != NULL && (rgt != NULL || lft != NULL)){
+        //  si tiene un hijo a la izquierda
+        if(lft != NULL){
+            //  si están mal ordenados
+            if(key_less(d->key, lft->key)){
+                return false;
+            }
         }
-        if (!invrep(lft)) return false;
-    }
-    
-    dict_t rgt = d->right;
-    if (rgt != NULL){
-        if (!key_less(d->key, rgt->key)) {
-            return false;
+        //  si tiene un hijo a la derecha
+        if(rgt != NULL){
+            //  si están mal ordenados
+            if(key_less(rgt->key, d->key)){
+                return false;
+            }
         }
-        if (!invrep(rgt)) return false;
+        //  si están bien ordenados se llama invrep para los hijos
+        return invrep(lft) && invrep(rgt);
     }
-
-    if (lft != NULL && lft->right != d) {
-        return false;
-    }
-
-    if (rgt != NULL && rgt->left != d) {
-        return false;
-    }
-
+    //  caso base de para todo es true
     return true;
 }
 
-//  declara un nodo en el diccionario, según lo que va a estar a su izquierda y su derecha
-static dict_t dict_declare( dict_t * left_ptr, dict_t * right_ptr, key_t key, value_t value){
+//  declara un nodo en el diccionario
+static dict_t node_declare(key_t key, value_t value){
     dict_t dict = NULL;
-    dict_t left = left_ptr != NULL? *left_ptr : NULL;
-    dict_t right = right_ptr != NULL? *right_ptr : NULL;
 
     //  se inicializa el diccionario
     dict = malloc(sizeof(struct _node_t));
-    dict->left = left;
-    dict->right = right;
+    dict->left = NULL;
+    dict->right = NULL;
     dict->key = key;
     dict->value = value;
-
-    //  a la derecha del nodo izquierdo, está el nuevo nodo
-    if(left != NULL){
-        left->right = dict;
-    }
-
-    //  a la izquierda del nodo derecho, está el nuevo nodo
-    if(right != NULL){
-        right->left = dict;
-    }
 
     return dict;
 }
 
 dict_t dict_empty(void) {
     dict_t dict = NULL;
-    
-    dict = dict_declare(NULL, NULL, NULL, NULL);
-
     assert(invrep(dict));
-
     return dict;
 }
 
 //  cambia la definición de una palabra
-static dict_t dict_add_exist(dict_t * dict_ptr, key_t word, value_t def){
+static dict_t dict_change_def(dict_t * dict_ptr, key_t word, value_t def){
     dict_t dict = *dict_ptr;
     
     //  si las palabras son iguales
     if(key_eq(word, dict->key)){
         //  se cambia la definicion
+        dict->key = key_destroy(dict->key);
+        dict->key = word;
         dict->value = value_destroy(dict->value);
         dict->value = def;
     
     //  si la palabra es menor a la actual
     }else if(key_less(word, dict->key)){
         //  se agrega a la izquierda
-        dict->left = dict_add_exist(&(dict->left), word, def);
+        dict->left = dict_change_def(&(dict->left), word, def);
     
     //  cc (si la palabra es mayor a la actual)
     } else{
         //  se agrega a la derecha
-        dict->right = dict_add_exist(&(dict->right), word, def);
+        dict->right = dict_change_def(&(dict->right), word, def);
     }
     return dict;    
 }
@@ -104,71 +94,34 @@ static dict_t dict_add_exist(dict_t * dict_ptr, key_t word, value_t def){
 //  añade un nodo con un nuevo key - value
 static dict_t dict_add_not_exist(dict_t * dict_ptr, key_t word, value_t def){
     dict_t dict = *dict_ptr;
-    
+
+    //  si el diccionario es nulo
+    if(dict == NULL){
+        //  se inicializa el diccionario
+        return node_declare(word, def);    
+    }
+
     //  si la palabra es menor a la actual
     if(key_less(word, dict->key)){
-
-        dict_t lft = dict->left;
-
-        //  si no hay nada a la izquierda, se agrega
-        if(lft == NULL){
-            dict->left = dict_declare(NULL, &dict, word, def);
-
-        //  si la palabra es menor a la de la izquierda
-        } else if (key_less(word, lft->key)){
-            //  se busca lugar a la izquierda
-            dict->left = dict_add_not_exist(&lft, word, def);
-        
-        //  cc (si la palabra es mayor a la de la izquierda)
-        } else{
-            //  se agrega en la siguiente posición a la izquierda
-            dict->left = dict_declare(&lft, &dict, word, def);               
-        }
+        //  se agrega a la izquierda
+        dict->left = dict_add_not_exist(&(dict->left), word, def);
     
-    // cc (si la palabra es mayor a la actual)
-    } else{
-        
-        dict_t rgt = dict->right;
-
-        //  si no hay nada a la derecha, se agrega
-        if(dict->right == NULL){
-            dict->right = dict_declare(&dict, NULL, word, def);
-        
-        //  si la palabra es mayor a la de la derecha
-        } else if(key_less(dict->right->key, word)){
-            //  se busca lugar a la derecha
-            dict->right = dict_add_not_exist(&(dict->right), word, def);
-        
-        //  cc (si la palabra es menor a la de la derecha)
-        } else{
-            //  se agrega en la siguiente posición a la derecha
-            dict->right = dict_declare(&dict, &rgt, word, def);
-        }
+    //  si la palabra es mayor a la actual
+    }else{
+        //  se agrega a la derecha
+        dict->right = dict_add_not_exist(&(dict->right), word, def);
     }
+    
     return dict;
 }
 
 dict_t dict_add(dict_t dict, key_t word, value_t def) {
     assert(invrep(dict) && word != NULL && def != NULL );
 
-    //  si el diccionario es vacío
-    if(dict_length(dict) == 0){
-        //  si el diccionario es nulo
-        if(dict == NULL){
-            //  se inicializa el diccionario
-            dict = dict_declare(NULL, NULL, word, def);
-        
-        //  si el diccionario no es nulo
-        }else{
-            //  se escriben los valores de la primera palabra
-            dict->key = word;
-            dict->value = def;
-        }
-
     //  si la palabra existe
-    }else if(dict_exists(dict, word)){
+    if(dict_exists(dict, word)){
         //  se cambia la definición
-        dict = dict_add_exist(&dict, word, def);
+        dict = dict_change_def(&dict, word, def);
     
     //  si la palabra no existe
     }else{
@@ -194,7 +147,7 @@ static value_t search_rec(dict_t * dict_ptr, key_t word){
         //  se busca a la izquierda
         def = search_rec(&(dict->left), word);
     
-    //  cc (la palabra buscada es mayors)
+    //  cc (la palabra buscada es mayor)
     }else{
         //  se busca a la derecha
         def = search_rec(&(dict->right), word);
@@ -241,102 +194,92 @@ static bool dict_exists_rec(dict_t * dict_ptr, key_t word){
 bool dict_exists(dict_t dict, key_t word) {
     assert(invrep(dict));
     
-    
-    if(dict_length(dict) != 0){
+    if(dict != NULL){
         return dict_exists_rec(&dict, word);
     }
 
     return false;
 }
 
-//  corregir
-unsigned int dict_length(dict_t dict) {
-    assert(invrep(dict));
-    unsigned int length = 0;
-    
-    //  si el diccionario no es nulo
-    if (dict != NULL) {
-        //  me voy todo a la izquierda posible
-        while(dict -> left != NULL){
-            dict = dict -> left;
-        }
-        //  cuento todos los de la derecha
-        while(dict != NULL){
-            if(dict -> key != NULL) length++;
-            dict = dict -> right;
-        }
+static unsigned int length_rec(dict_t * dict_ptr){
+    dict_t dict = *dict_ptr;
+    unsigned int length = 0u;
+    if (dict != NULL){
+        //  se cuenta el nodo, se cuentan los nodos izq, se cuentan los nodos derechos
+        length++;
+        length += length_rec(&(dict->left));
+        length += length_rec(&(dict->right));
     }
     return length;
+}
+
+unsigned int dict_length(dict_t dict) {
+    assert(invrep(dict));
+    
+    unsigned int length = 0u;
+    if (dict != NULL){
+        length = length_rec(&dict);    
+    }
+    
+    assert(invrep(dict) && (dict == NULL || length > 0));
+    return length;
+}
+
+static tuple dict_min(dict_t dict){
+    while (dict->left != NULL){
+        dict = dict->left;
+    }
+    tuple min;
+    min.key = dict->key;
+    min.value = dict->value;
+    return min;
 }
 
 //  uso direcciones para evitar memory leaking
 static dict_t remove_rec(dict_t * dict_ptr, key_t word){
     dict_t dict = *dict_ptr;
+    dict_t lft = dict != NULL ?dict->left : NULL;
+    dict_t rgt = dict != NULL ?dict->right: NULL;
 
-    //  si las palabras son iguales
-    if (key_eq(word, dict->key)){
-        //  si no hay nada a izquierda o derecha
-        if(dict->right == NULL && dict->left == NULL){
-            
-            
-            //  se libera la memoria
-            dict->key = key_destroy(dict->key);
-            dict->value = value_destroy(dict->value);
-            free(dict);
-            
-            return NULL;
-        
-        //  si no hay nada a la izquierda
-        } else if(dict -> left == NULL){
-            dict_t rgt = dict -> right;
-            
-            //  se libera la memoria
-            dict->key = key_destroy(dict->key);
-            dict->value = value_destroy(dict->value);
-            free(dict);
-            
-            //  rgt es el nuevo dict
-            return rgt;
+    //  la palabra es menor a la raiz
+    if (key_less(word, dict->key)){
+        //  se borra de la izquierda
+        dict -> left = remove_rec(&lft, word);
 
-        //  si no hay nada a la derecha
-        } else if( dict -> right == NULL){
-            dict_t lft = dict -> left;
-            
-            //  se libera la memoria
-            dict->key = key_destroy(dict->key);
-            dict->value = value_destroy(dict->value);
-            free(dict);
-            
-            //  dict apunta a lft
-            return lft;
-
-        //  si hay algo en ambas posiciones
-        } else{
-            dict_t lft = dict->left, rgt = dict->right;
-
-            //  a la izquierda de rgt está lft
-            rgt -> left = lft;
-            //  el nodo lft todavía no apunta a right
-            lft -> right = rgt;
-            
-            //  se libera la memoria
-            dict->key = key_destroy(dict->key);
-            dict->value = value_destroy(dict->value);
-            free(dict);
-            
-            //  dict apunta a lft
-            return lft;
-        }
-
-    //  si la palabra buscada es menor
-    } else if(key_less(word, dict->key)){
-        //  se busca a   la izquierda
-        dict -> left = remove_rec(&dict->left, word);
+    //  la palabra es mayor a la raiz
+    }else if(key_less(dict->key, word)){
+        //  se borra de la derecha
+        dict -> right = remove_rec(&rgt, word);
     
-    //  cc (la palabra buscada es mayor)
-    } else{
-        //  se busca a la derecha
-        dict -> right = remove_rec(&dict->right, word);
+    //  word es el elemento a borrar
+    }else{
+        //  si no hay elemento a la izquierda
+        if(lft == NULL){
+            dict->key = key_destroy(dict->key);
+            dict->value = value_destroy(dict->value);
+            free(dict);
+            dict = rgt;
+
+        //  si no hay elemento a la derecha
+        } else if(rgt == NULL){
+            dict->key = key_destroy(dict->key);
+            dict->value = value_destroy(dict->value);
+            free(dict);
+            dict = lft;
+
+        //  si hay elementos a derecha e izquierda
+        } else{
+            //  se reeplaza el nodo con el elemento mínimo de la derecha
+            tuple rgt_min = dict_min(rgt);
+            dict -> value = value_destroy(dict->value);
+            dict -> key = key_destroy(dict->key);
+            dict -> key = rgt_min.key;
+            dict -> value = rgt_min.value;
+
+            //  se elimina el elemento que se usó previamente
+            dict -> right = remove_rec(&(dict -> right), rgt_min.key);  
+        
+        }
     }
     return dict;
 }
@@ -354,13 +297,16 @@ dict_t dict_remove(dict_t dict, key_t word) {
 
 static dict_t remove_all_rec(dict_t * dict_ptr){
     dict_t dict = *dict_ptr;
+    if (dict!= NULL){
+        remove_all_rec(&dict->left);
+        remove_all_rec(&dict->right);
     
-    remove_all_rec(&dict->left);
-    remove_all_rec(&dict->right);
+        dict->key = key_destroy(dict->key);
+        dict->value = value_destroy(dict->value);
+        free(dict);
+        dict_ptr = NULL;
+    }
     
-    dict->key = key_destroy(dict->key);
-    dict->value = value_destroy(dict->value);
-    free(dict);
     
     return NULL;
 }
@@ -369,21 +315,30 @@ dict_t dict_remove_all(dict_t dict) {
     assert(invrep(dict));
     
     dict = remove_all_rec(&dict);
-    
+    dict = NULL;
+
     return dict;
+}
+
+static void dump_rec(dict_t * dict_ptr, FILE * file){
+    dict_t dict = *dict_ptr;
+    if(dict != NULL){
+        dump_rec(&(dict->left), file);
+
+        key_dump(dict->key, file);
+        fprintf(file, ": ");
+        value_dump(dict->value, file);
+        fprintf(stdout, "\n");
+
+        dump_rec(&(dict->right), file);
+    }
 }
 
 void dict_dump(dict_t dict, FILE *file) {
     assert(invrep(dict));
-    while (dict->left != NULL){
-        dict = dict->left;
-    }
-    while (dict != NULL){
-        key_dump(dict->key, file);
-        fprintf(file, ": ");
-        value_dump(dict->value, file);
-        fprintf(file, "\n");
-        dict = dict->right;
+    
+    if (dict != NULL){
+        dump_rec(&dict, file);
     }
     
 }
@@ -397,4 +352,3 @@ dict_t dict_destroy(dict_t dict) {
 
     return dict;
 }
-
